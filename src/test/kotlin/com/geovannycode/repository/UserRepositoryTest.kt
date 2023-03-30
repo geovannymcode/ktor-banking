318c18f9d2ac425b2b681e5f7cd62031d8d7e6dc
Geovanny Mendoza <geovanny0401@gmail.com>
2023-03-29 23:33:50 -0500

    [V-4]Added UserRepository test

--- src/test/kotlin/com/geovannycode/repository/UserRepositoryTest.kt
+++ src/test/kotlin/com/geovannycode/repository/UserRepositoryTest.kt
@@ -5,7 +5,9 @@ import com.geovannycode.di.bankingAppModule
 import com.geovannycode.entities.user.UserEntity
 import com.geovannycode.entities.user.UserTable
 import com.geovannycode.models.User
+import io.ktor.util.reflect.*
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -16,6 +18,8 @@ import org.koin.test.inject
 import org.koin.test.junit5.KoinTestExtension
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.ZoneOffset.UTC
 import java.util.*
 
 internal class UserRepositoryTest: KoinTest {
@@ -58,5 +62,114 @@ internal class UserRepositoryTest: KoinTest {
 
         assertThat(current).isNotNull
         assertThat(transaction { UserEntity.find { UserTable.userId eq current.userId }.single() }).isNotNull
+    }
+
+    @Test
+    fun `save updates existing user to database`() {
+        val user = User(
+            userId = UUID.randomUUID(),
+            firstName = "Geovanny",
+            lastName = "Mendoza",
+            birthdate = LocalDate.of(2000,1,1),
+            password = "test",
+            created = LocalDateTime.of(2023,1,1,1,0,0),
+            lastUpdated = LocalDateTime.of(2023,1,2,1,0,0),
+            accounts = listOf()
+        )
+        val current = userRepository.save(user)
+
+        userRepository.save(
+            current.copy(
+                firstName = "Manuel"
+            )
+        )
+
+        assertThat(current).isNotNull
+        assertThat(transaction { UserEntity.all().count()}).isEqualTo(1)
+        assertThat(transaction { UserEntity.find { UserTable.userId eq current.userId }.single().firstName}).isEqualTo("Manuel")
+    }
+
+    @Test
+    fun `delete removes existing user from database`() {
+        val user = User(
+            userId = UUID.randomUUID(),
+            firstName = "Geovanny",
+            lastName = "Mendoza",
+            birthdate = LocalDate.of(2000,1,1),
+            password = "test",
+            created = LocalDateTime.of(2023,1,1,1,0,0),
+            lastUpdated = LocalDateTime.of(2023,1,2,1,0,0),
+            accounts = listOf()
+        )
+
+        val userNew = userRepository.save(user)
+
+        userRepository.delete(userNew)
+
+        assertThat(transaction { UserEntity.find { UserTable.userId eq user.userId}.singleOrNull() }).isNull()
+    }
+
+    @Test
+    fun `delete throws exception if try to delete user without id`() {
+        val user = User(
+            userId = UUID.randomUUID(),
+            firstName = "Geovanny",
+            lastName = "Mendoza",
+            birthdate = LocalDate.of(2000,1,1),
+            password = "test",
+            created = LocalDateTime.of(2023,1,1,1,0,0),
+            lastUpdated = LocalDateTime.of(2023,1,2,1,0,0),
+            accounts = listOf()
+        )
+
+        assertThatThrownBy { userRepository.delete(user) }.instanceOf(IllegalStateException::class)
+    }
+
+    @Test
+    fun `delete throws exception if try to delete user with not existing id`() {
+        val user = User(
+            userId = UUID.randomUUID(),
+            firstName = "Geovanny",
+            lastName = "Mendoza",
+            birthdate = LocalDate.of(2000,1,1),
+            password = "test",
+            created = LocalDateTime.of(2023,1,1,1,0,0),
+            lastUpdated = LocalDateTime.of(2023,1,2,1,0,0),
+            accounts = listOf()
+        )
+
+        assertThatThrownBy { userRepository.delete(user) }.instanceOf(IllegalStateException::class)
+    }
+
+    @Test
+    fun `findByUserId returns matching user`() {
+        val user = User(
+            userId = UUID.randomUUID(),
+            firstName = "Geovanny",
+            lastName = "Mendoza",
+            birthdate = LocalDate.of(2000,1,1),
+            password = "test",
+            created = LocalDateTime.of(2023,1,1,1,0,0),
+            lastUpdated = LocalDateTime.of(2023,1,2,1,0,0),
+            accounts = listOf()
+        )
+
+        val userNew = userRepository.save(user)
+        val current = userRepository.findByUserId(userNew.userId)
+        assertThat(current).isNotNull
+        current?.run{
+            assertThat(this.userId).isEqualTo(userNew.userId)
+            assertThat(this.firstName).isEqualTo(userNew.firstName)
+            assertThat(this.lastName).isEqualTo(userNew.lastName)
+            assertThat(this.created.toEpochSecond(UTC)).isEqualTo(
+                userNew.created.toEpochSecond(UTC)
+            )
+            assertThat(this.lastUpdated.toEpochSecond(ZoneOffset.UTC)).isEqualTo(
+                userNew.lastUpdated.toEpochSecond(
+                    ZoneOffset.UTC
+                )
+            )
+            assertThat(this.accounts).isEmpty()
+        }
     }
 }
